baf8e7e2d292def5f2805764a23adb2720c8c724
kyomi <personal@kyomi.codes>
2022-12-26 17:40:58 -0300

    fix(migrations): fix wrong import

--- migrations/script.py.mako
+++ migrations/script.py.mako
@@ -5,7 +5,7 @@ Revises: ${down_revision | comma,n}
 Create Date: ${create_date}
 """
 
-from alembic.op import create_table, drop_column
+from alembic.op import create_table, drop_table
 from sqlalchemy import *
 ${imports if imports else ""}
 
--- migrations/versions/66f698bbb3a5_create_levels_table.py
+++ migrations/versions/66f698bbb3a5_create_levels_table.py
@@ -5,9 +5,8 @@ Revises:
 Create Date: 2022-12-23 19:11:28.997018
 """
 
-from alembic.op import create_table, drop_column
-from sqlalchemy import Column, BigInteger
-
+from alembic.op import create_table, drop_table
+from sqlalchemy import BigInteger, Column
 
 # revision identifiers, used by Alembic.
 revision = "66f698bbb3a5"
@@ -25,4 +24,4 @@ def upgrade():
 
 
 def downgrade():
-    drop_column("levels")
+    drop_table("levels")
